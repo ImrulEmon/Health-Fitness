0dbad616af8dcb393ba23de8e604c956aba7c154
Imrul Emon <emon_wb@Imruls-Mac-mini.local>
2022-11-21 16:43:36 +0600

    Home

--- lib/widgets/category_card_widget.dart
+++ lib/widgets/category_card_widget.dart
@@ -20,11 +20,20 @@ class CategoryCardWidget extends StatelessWidget {
       width: 142.0,
       alignment: Alignment.topCenter,
       padding: const EdgeInsets.only(top: paddingDft),
-      margin: const EdgeInsets.symmetric(horizontal: paddingDft * .5),
+      margin: const EdgeInsets.symmetric(
+          horizontal: paddingDft * .5, vertical: paddingDft),
       decoration: BoxDecoration(
         color: bgColor,
         // border: Border.all(),
         borderRadius: BorderRadius.circular(24.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.8),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(2, 5),
+          ),
+        ],
         image: DecorationImage(
           image: AssetImage(bgImg),
           fit: BoxFit.cover,
